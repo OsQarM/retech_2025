5eb98c499cd87705a36064d8bc2a22c37548b940
OsQarM <oscar.michel@qilimanjaro.tech>
2025-11-13 17:59:23 +0100

    Added qutip version of Hamiltonian

--- src/hamiltonian.py
+++ src/hamiltonian.py
@@ -1,5 +1,77 @@
+import qutip as qt
+import numpy as np
 
-class Hamiltonian():
+
+sx = qt.sigmax()
+sy = qt.sigmay()
+sz = qt.sigmaz()
+
+class QiliSDKHamiltonian():
+    
+    def __init__(self,size):
+        self.size = size
+        self.H = 0
+    
+    def add_local_field(self, qubit, field, weight=1.):
+
+        assert qubit <= self.size-1, f"Index {qubit} out of range. Size of the system is {self.size}"
+        
+        self.H += weight*field(qubit)
+
+    def add_ZZ_term(self, qubit1, qubit2, weight=1.):
+
+        assert qubit1<= self.size-1, f"Index {qubit1} out of range. Size of the system is {self.size}"
+        assert qubit2 <= self.size-1, f"Index {qubit2} out of range. Size of the system is {self.size}"
+        assert qubit1 != qubit2,    f"Qubits 1 and 2 are the same, cannot apply self-interaction term"
+
+        self.H += weight*Z(qubit1)*Z(qubit2)
+
+
+class QutipHamiltonian():
+
+    def __init__(self,size):
+        self.size = size
+        self.H = 0 
+        self.sx_list, self.sy_list, self.sz_list = self._initialize_operators()
+
+    def _initialize_operators(self):
+        '''Setup operators for individual qubits
+           for each value of i it puts the paulis in different positions of the list, 
+           then does IxIxI...sigma_ixIxI...xI
+        '''
+        sx_list, sy_list, sz_list = [], [], []
+        for i in range(self.size):
+            #list of 2x2 identity matrices
+            op_list = [qt.qeye(2)] * self.size
+            #replace i-th element with sigma_x
+            op_list[i] = sx
+            #create matrices of 2^Nx2^N
+            sx_list.append(qt.tensor(op_list))
+            #do the same for sigma_y and sigma_z
+            op_list[i] = sy
+            sy_list.append(qt.tensor(op_list))
+            op_list[i] = sz
+            sz_list.append(qt.tensor(op_list))
+
+        return sx_list, sy_list, sz_list 
+    
+    def add_x_field(self, qubit, weight=1.):
+
+        assert qubit <= self.size-1, f"Index {qubit} out of range. Size of the system is {self.size}"
+        self.H += self.sx_list[qubit]
+
+    def add_z_field(self, qubit, weight=1.):
+
+        assert qubit <= self.size-1, f"Index {qubit} out of range. Size of the system is {self.size}"
+        self.H += self.sz_list[qubit]
+
+    def add_ZZ_term(self, qubit1, qubit2, weight=1.):
+
+        assert qubit1<= self.size-1, f"Index {qubit1} out of range. Size of the system is {self.size}"
+        assert qubit2 <= self.size-1, f"Index {qubit2} out of range. Size of the system is {self.size}"
+        assert qubit1 != qubit2,    f"Qubits 1 and 2 are the same, cannot apply self-interaction term"
+
+        self.H += self.sz_list[qubit1]*self.sz_list[qubit2]
+    
 
     
-    pass
