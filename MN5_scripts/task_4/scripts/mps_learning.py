44799e0c6ad10640365fa727c11626ea6714d1a2
OsQarM <oscar.michel@qilimanjaro.tech>
2026-04-08 17:44:31 +0200

    Minor bug fixing

--- MN5_scripts/task_4/scripts/mps_learning.py
+++ MN5_scripts/task_4/scripts/mps_learning.py
@@ -439,8 +439,6 @@ def bar_plot_strings_comparison(strings, values1, values2, config, labels=None,
     return fig, ax, (bars1, bars2) if style != 'stacked' else (bars1, bars2)
 
 
-
-
 def plot_training_loss(losses, config):
     plt.figure(figsize=(5,4))
     plt.plot(list(range(1, len(losses)+1)), losses)
@@ -1263,12 +1261,11 @@ class OperatorClass:
        The operators will be applied to each qubit, and we will allow for the construction of any
        combination of Pauli strings 
     '''
-    def __init__(self, L, dtype=torch.complex64):
+    def __init__(self, L, topology = "linear", dtype=torch.complex64):
 
         self.L = L
         self.topology = topology # "linear" or "star"
         self.dim = 2**L
-        self.topology = topology # "linear" or "star"
         self.pauli_basis = {}
         self.pauli_basis['X'], self.pauli_basis['Y'], self.pauli_basis['Z'], self.pauli_basis['I'] = paulis(dtype)
         self.operators = []
@@ -1295,7 +1292,6 @@ class OperatorClass:
                 #Create identity operators for each qubit
                 ops = [self.pauli_basis['I']]*self.L
 
-                if self.topology == "linear":
     
                 if self.topology == "linear":
                     for j, char in enumerate(pauli_string):
@@ -1303,18 +1299,6 @@ class OperatorClass:
                         #At each iterations the string displaces one site starting at i.
                         #At each iterations the string displaces one site
                            ops[i+j] = self.pauli_basis[char]
-
-                elif self.topology == "star":
-                    #Puts first operator in center
-                    ops[0] = self.pauli_basis[pauli_string[0]]
-                    #Starts putting in qubits around. 
-                    # As i increases it skips the qubits where operators have been placed
-                    for j, char in enumerate(pauli_string[1:]):
-                        ops[i+1+j] = self.pauli_basis[char]
-
-                else:
-                    raise ValueError(f"Topology {self.topology} invalid. Choose 'linear' or 'star'.")
-
 
                 elif self.topology == "star":
                     if len(pauli_string) == 1:
@@ -1632,7 +1616,7 @@ if __name__ == "__main__":
     psi0 = prepare_initial_state(L, initial_state_kind)
     
     # Initialize Hamiltonian operators
-    OPS_LIST = OperatorClass(L, topology="linear", topology="star")
+    OPS_LIST = OperatorClass(L, topology="linear")
     OPS_LIST.add_operators('ZZ')
     OPS_LIST.add_operators('X')
     OPS_LIST.add_operators('Z')
